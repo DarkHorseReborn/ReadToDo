6ea997ed06a1e03f7e0409ac40d02a7232c72904
Issac Ong <issacong28@gmail.com>
2019-10-02 02:19:19 +0800

    Read JS - Part 1

---
+++ ReadToDo/ReadToDo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ReadToDo
+{
+    class Program
+    {
+        const string TODO = "TODO";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your directory:");
+            string fileDirectory = Console.ReadLine();
+            GetFiles(fileDirectory);
+        }
+
+        private static void GetFiles(string fileDirectory)
+        {
+            string[] files = Directory.GetFiles(fileDirectory, "*.js", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                using (FileStream fileStream = File.OpenRead(file))
+                {
+                    byte[] fileContents = new byte[fileStream.Length];
+                    UTF8Encoding decoding = new UTF8Encoding(true);
+
+                    while (fileStream.Read(fileContents, 0, fileContents.Length) > 0)
+                    {
+                        string content = decoding.GetString(fileContents);
+
+                        if (content.Contains(TODO))
+                        {
+                            Console.WriteLine(file);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
